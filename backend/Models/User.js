641f6a9a09c5209d958daef0dbdbd3c2a96f958b
Abrham <abrsh6266@gmail.com>
2024-09-03 04:33:31 -0800

    initial commit with db configs

---
+++ backend/Models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'instructor'],
+    default: 'instructor',
+  },
+}, { timestamps: true });
+
+// Hashing password
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Compare
+userSchema.methods.comparePassword = async function(pwd) {
+  return await bcrypt.compare(pwd, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
